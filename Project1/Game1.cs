8380db70abb29783ed215c319c61e953c35d1ef6
Mark Sherriff <sherriff@virginia.edu>
2023-02-07 22:18:41 -0500

    Add project files.

---
+++ Project1/Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Rectangle Player;
+
+        public Texture2D Texture;
+
+        private const int speed = 5;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            if (Texture == null)
+            {   //create texture to draw with if it does not exist
+                Texture = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+                Texture.SetData<Color>(new Color[] { Color.White });
+            }
+
+            Player = new Rectangle(10, 150, 20, 20);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (GamePad.GetState(PlayerIndex.One).DPad.Up == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                Player.Y -= speed;
+            }
+            if (GamePad.GetState(PlayerIndex.One).DPad.Down == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Down))   
+            {
+                Player.Y += speed;
+            }
+            if (GamePad.GetState(PlayerIndex.One).DPad.Right == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                Player.X += speed;
+            }
+            if (GamePad.GetState(PlayerIndex.One).DPad.Left == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                Player.X -= speed;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp);
+
+            DrawRectangle(_spriteBatch, Player, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawRectangle(SpriteBatch sb, Rectangle Rec, Color color)
+        {
+            Vector2 pos = new Vector2(Rec.X, Rec.Y);
+            sb.Draw(Texture, pos, Rec,
+                color * 1.0f,
+                0, Vector2.Zero, 1.0f,
+                SpriteEffects.None, 0.00001f);
+        }
+    }
+}
